8bb8ab2354c2336e33b2fb9e1fb3c2121974928f
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-02-06 07:29:34 +0800

    Update show-plugin-outlets.js

--- javascripts/discourse/components/show-plugin-outlets.js
+++ javascripts/discourse/components/show-plugin-outlets.js
@@ -6,6 +6,7 @@ export default class ShowPluginOutlets extends Component {
   @action
   show_plugin_outlets_action() {
     let devToolsShowing = localStorage.getItem('devToolsShowing');
+    console.log(localStorage.getItem('devToolsShowing'));
     if (devToolsShowing) {
       localStorage.setItem('devToolsShowing', false);
       disableDevTools();
@@ -13,7 +14,7 @@ export default class ShowPluginOutlets extends Component {
     } else {
       localStorage.setItem('devToolsShowing', true);
       console.log("Enabling")
-      console.log(localStorage.getItem('devToolsShowing'));
+      
       enableDevTools();
 
     }
