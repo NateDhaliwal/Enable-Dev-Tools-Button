898fac17bf85c589ca33db42e60218731cac59b2
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-02-05 19:04:26 +0800

    Create show-plugin-outlets.js

---
+++ javascripts/discourse/components/show-plugin-outlets.js
@@ -0,0 +1,19 @@
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+import { tracked } from '@glimmer/tracking';
+
+export default class ShowPluginOutlets extends Component {
+  @tracked devToolsShowing = false;
+  
+  @action
+  show_plugin_outlets_action() {
+    if (this.devToolsShowing) {
+      disableDevTools();
+      this.devToolsShowing = false;
+    } else {
+      enableDevTools();
+      this.devToolsShowing = true;
+    }
+    
+  }
+}
