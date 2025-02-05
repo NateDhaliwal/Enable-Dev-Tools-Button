b58b8efbf6926de2d269e498dadf143441e93371
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-02-06 07:35:46 +0800

    Update show-plugin-outlets.js

--- javascripts/discourse/components/show-plugin-outlets.js
+++ javascripts/discourse/components/show-plugin-outlets.js
@@ -5,7 +5,7 @@ export default class ShowPluginOutlets extends Component {
 
   @action
   show_plugin_outlets_action() {
-    let devToolsShowing = localStorage.getItem('devToolsShowing');
+    let devToolsShowing = localStorage.setItem('devToolsShowing', false);
     console.log(localStorage.getItem('devToolsShowing'));
     if (devToolsShowing) {
       console.log('SHowing');
