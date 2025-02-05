7464be90219c4946f02602503e3a7048368d7dd7
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-02-05 19:42:43 +0800

    Update show-plugin-outlets.js

--- javascripts/discourse/components/show-plugin-outlets.js
+++ javascripts/discourse/components/show-plugin-outlets.js
@@ -3,21 +3,23 @@ import { action } from "@ember/object";
 import { tracked } from '@glimmer/tracking';
 
 export default class ShowPluginOutlets extends Component {
+  @tracked buttonIcon = localStorage.getItem('buttonIcon');
   @action
   show_plugin_outlets_action() {
     let devToolsShowing = localStorage.getItem('devToolsShowing');
     let buttonIcon = localStorage.getItem('buttonIcon');
     console.log(localStorage.getItem('devToolsShowing'));
+    console.log(localStorage.getItem('buttonIcon'));
     if (devToolsShowing) {
       localStorage.setItem('devToolsShowing', false);
       localStorage.setItem('buttonIcon', "bug-slash");
-      this.args.buttonIcon = buttonIcon;
+      this.buttonIcon = buttonIcon;
       disableDevTools();
       
     } else {
       localStorage.setItem('devToolsShowing', true);
       localStorage.setItem('buttonIcon', "bugh");
-      this.args.buttonIcon = buttonIcon;
+      this.buttonIcon = buttonIcon;
       enableDevTools();
       
     }
