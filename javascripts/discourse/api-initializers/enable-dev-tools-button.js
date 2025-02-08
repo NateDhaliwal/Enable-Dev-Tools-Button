63089c080bd7f1f10a1628ac5da823c5c6707f4e
NateDhaliwal <143097364+NateDhaliwal@users.noreply.github.com>
2025-02-08 16:03:35 +0800

    Update enable-dev-tools-button.js

--- javascripts/discourse/api-initializers/enable-dev-tools-button.js
+++ javascripts/discourse/api-initializers/enable-dev-tools-button.js
@@ -3,21 +3,22 @@ import { apiInitializer } from "discourse/lib/api";
 
 
 export default apiInitializer("1.14.0", (api) => {
-  let currentUser = api.getCurrentUser();
-  console.log(currentUser);
-  if (settings.admin_only) {
-    if (currentUser.admin) {
-      api.headerIcons.add(
+  function displayButton() {
+    api.headerIcons.add(
         'eye',
         ShowPluginOutlets,
         { before: "search" }
-      );
+  }
+  
+  let currentUser = api.getCurrentUser();
+  if (settings.logged_in_only && currentUser) {
+    displayButton()
+  }
+  if (settings.admin_only) {
+    if (currentUser.admin) {
+      displayButton()
     }
   } else {
-    api.headerIcons.add(
-      'eye',
-      ShowPluginOutlets,
-      { before: "search" }
-    );
+    displayButton();
   }
 });
